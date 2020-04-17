266dbd4bbfb3d525d5b1b37d92982b12bb1d8357
Jens <github@jinix.de>
2020-04-17 17:36:42 +0200

    Update stack name; add s3 bucket

--- app.py
+++ app.py
@@ -6,6 +6,6 @@ from hello_cdk.hello_cdk_stack import HelloCdkStack
 
 
 app = core.App()
-HelloCdkStack(app, "hello-cdk")
+HelloCdkStack(app, "HelloCdkStack")
 
 app.synth()
--- hello_cdk/hello_cdk_stack.py
+++ hello_cdk/hello_cdk_stack.py
@@ -1,9 +1,13 @@
-from aws_cdk import core
-
+from aws_cdk import (
+    aws_s3 as s3,
+    core
+)
 
 class HelloCdkStack(core.Stack):
 
     def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
-        # The code that defines your stack goes here
+        bucket = s3.Bucket(self, 
+            "jinixdme_cdk_deploy", 
+            versioned=True,)
